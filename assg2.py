3f150f5513778de8f8d0c54371ea42dd0707ce72
Luobin-Zeng <q2721340562@gmail.com>
2019-11-19 20:38:22 -0500

    u

--- assg2.py
+++ assg2.py
@@ -352,6 +352,10 @@ global DOOR_APW1
 DOOR_APW1 = -1
 DOOR_APW = tk.DoubleVar()
 
+
+
+
+
 def update_dict(string, value):
     string = string
     value = value
@@ -371,7 +375,7 @@ def ini_file():
     
     with open('test_data.txt','w') as json_file:
         json.dump(data_dict, json_file) #the data_dict is now converted to JSON string 
-
+ini_file()
 #Initializing the data_dict (IMPORTANT) AND USER LIMITS
 global data_dict
 
@@ -496,14 +500,13 @@ def Button():
 
 
     def M_VOO():
-        window_mode_voo = tk.Toplevel(window_mode_selection)
+        window_mode_voo = tk.Toplevel(window)
         window_mode_voo.geometry('500x500')
         window_mode_voo.title('VOO is selected!')
     
-        
+        VOO_LRL1 = VOO_LRL.get()
         LRL_value = tk.Entry(window_mode_voo, textvariable = VOO_LRL, font=('Arial',12))
         LRL_value.place(x=280,y=175)
-        VOO_LRL1 = VOO_LRL.get()
         tk.Label(window_mode_voo, text='Lower Rate Limit(Int)(30ppm-175ppm): ').place(x=30,y=175)
         update_dict('VOO_LRL',VOO_LRL1)
         
@@ -526,26 +529,33 @@ def Button():
         VPW_value.place(x=280,y=265)
         tk.Label(window_mode_voo, text='Ventricular Pulse Width(Float)(0.1ms-1.9ms): ').place(x=30,y=265)
         update_dict('VOO_VPW',VOO_VPW1)
-
-        def M_VOO_compare():
-            update_dict('VOO_LRL',VOO_LRL1)
-            if (not(30<VOO_LRL1<175)):
-                tkinter.messagebox.showerror('Error','LRL out of range!')
-            elif (not(50<VOO_URL1<175)):
-                tkinter.messagebox.showerror('Error','URL out of range!')
-            elif (not(3.5<VOO_VA1<7)):
-                tkinter.messagebox.showerror('Error','VA out of range!')
-            elif (not(1<VOO_VPW1<10)):
-                tkinter.messagebox.showerror('Error','VPW out of range!')
-            else:
-                tkinter.messagebox.showinfo('Passed','Values saved!')
-        VOO_Com = tk.Button(window_mode_voo, text='VOO_COMPARE', command=M_VOO_compare)
+        VOO_Com = tk.Button(window_mode_voo, text='VOO_COMPARE', command=M_VOO)
         VOO_Com.place(x=50,y=300)
-
-    
-      
         
-            
+
+        if VOO_LRL1 == 0:
+            pass
+        else:
+            if VOO_LRL1<30 or VOO_LRL1>175:
+                tkinter.messagebox.showerror('Error','LRL out of range!',parent=window_mode_voo)
+                
+            elif VOO_URL1<50 or VOO_URL1>175:
+                tkinter.messagebox.showerror('Error','URL out of range!',parent=window_mode_voo)
+                
+            elif VOO_VA1<3.5 or VOO_VA1>7:
+                tkinter.messagebox.showerror('Error','VA out of range!',parent=window_mode_voo)
+                
+            elif VOO_VPW1<0.1 or VOO_VPW1>1.9:
+                tkinter.messagebox.showerror('Error','VPW out of range!',parent=window_mode_voo)
+                
+            else:
+                tkinter.messagebox.showinfo('Passed','Values Saved!',parent=window_mode_voo)
+            window_mode_voo.destroy()
+  
+     
+
+        
+      
             
             
         
